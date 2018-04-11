8c55d387d167738e75e47090b26d343bdb513be7
Neelima Nair <neelimarun@gmail.com>
2018-04-11 08:03:07 +0800

    Committing JSON and firebase configurations

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyDr-SrP4QUu2L4EQFcvzOjRIct1ECeR4G0",
+    authDomain: "finder-7c3f1.firebaseapp.com",
+    databaseURL: "https://finder-7c3f1.firebaseio.com",
+    projectId: "finder-7c3f1",
+    storageBucket: "finder-7c3f1.appspot.com",
+    messagingSenderId: "847677399379"
+  };
